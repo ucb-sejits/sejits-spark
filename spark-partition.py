a7eda734b815a2f13026dae5f422a6582c30001e
chengstephen <stephen.cheng2013@gmail.com>
2015-08-03 17:23:38 -0700

    Update spark-partition.py

--- spark-partition.py
+++ spark-partition.py
@@ -13,9 +13,7 @@ from itertools import *
 sc = SparkContext("local[8]")
 sc.setLogLevel("WARN")
 
-# Specify if data is 2-D or 3-D
-type_of_data = 2
-
+"""SETUP"""
 length = 8
 width = 8
 depth = 8
@@ -23,19 +21,22 @@ depth = 8
 twoD_partitions = 4
 # 3-D Partition numbers should preferably be perfect cubes
 threeD_partitions = 8
+ITERATIONS = 2
+twoD_array_data = np.random.randint(5, size = (length+2*ITERATIONS,width+2*ITERATIONS))
+threeD_array_data = np.random.randint(5, size = (depth+2*ITERATIONS,length+2*ITERATIONS,width+2*ITERATIONS))
 
+
+"""Calculations"""
 # If this is able to be square rooted to an integer, a proper partition can be created
 twoD_partition_dimension = round(math.sqrt(length*width/twoD_partitions),0)
-print "2-D Partition Dimension:", twoD_partition_dimension
+# print "2-D Partition Dimension:", twoD_partition_dimension
+
 # If this is able to be cube rooted to an integer, a proper partition can be created
 threeD_partition_dimension = round(math.pow(length*width*depth/threeD_partitions,1.0/3.0),0)
-print "2-D Partition Dimension:", threeD_partition_dimension
+# print "2-D Partition Dimension:", threeD_partition_dimension
 
-ITERATIONS = 2
 
-# Testing on 2-D array
-twoD_array_data = np.random.randint(5, size = (length+2*ITERATIONS,width+2*ITERATIONS)) # Add 2*ITERATIONS to both length and width for ghost buffer
-print twoD_array_data
+"""2-D array"""
 partitioned_twoD_data = np.empty([twoD_partitions,twoD_partition_dimension+2*ITERATIONS,twoD_partition_dimension+2*ITERATIONS])
 left_bound, top_bound = 0, 0
 for i in range(1,twoD_partitions+1):
@@ -49,10 +50,10 @@ for i in range(1,twoD_partitions+1):
 		left_bound = left_bound+twoD_partition_dimension
 twoD_jacobi_rdd = sc.parallelize(partitioned_twoD_data, twoD_partitions)
 print twoD_jacobi_rdd.glom().collect()
+#Run SEJITS here
 
-#Testing on 3-D array
-threeD_array_data = np.random.randint(5, size = (depth+2*ITERATIONS,length+2*ITERATIONS,width+2*ITERATIONS))
-print threeD_array_data
+
+"""3-D array"""
 partitioned_threeD_data = np.empty([threeD_partitions,threeD_partition_dimension+2*ITERATIONS,threeD_partition_dimension+2*ITERATIONS
 									,threeD_partition_dimension+2*ITERATIONS])
 left_bound, top_bound, front_bound = 0, 0, 0
@@ -72,3 +73,4 @@ for i in range(1,threeD_partitions+1):
 		left_bound = left_bound+threeD_partition_dimension
 threeD_jacobi_rdd = sc.parallelize(partitioned_threeD_data, threeD_partitions)
 print threeD_jacobi_rdd.glom().collect()
+#Run SEJITS here
